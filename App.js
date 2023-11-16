24b71474f633fc59fc06756cc0d4b11afde94e45
Gabe dailey <gabedailey@Gabes-MacBook-Air.local>
2023-11-16 04:34:08 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/Shop'
+import LoginSignup from './Pages/LoginSignup';
+import Cart from './Pages/Cart';
+import Product from './Pages/Product';
+function App() {
+  return (
+    <div>
+      <BrowserRouter>     
+      <Navbar/>
+      <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category='mens'/>}/>
+      <Route path='/womens' element={<ShopCategory category='women'/>}/>
+      <Route path='/about' element={<ShopCategory category='about'/>}/>
+      <Route path='/product' element={<Product/>}>
+         <Route path = ':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
